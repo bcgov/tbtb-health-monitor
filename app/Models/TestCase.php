5ae2c4c7d14c1e4ad7ebdcf8dc52011e0fac3d24
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-04-01 12:41:11 -0700

    removes clean php

--- app/Models/TestCase.php
+++ app/Models/TestCase.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TestCase extends Model
 {
-    use HasFactory;
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at'];
 
     public function contacts(){
         return $this->belongsToMany('App\Models\Contact', 'contact_test_case', 'test_case_id', 'contact_id');
