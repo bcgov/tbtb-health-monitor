664d941e116e5ab3e5e94c96c9be4eedfe6a36ac
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-04-11 11:47:46 -0700

    updates probe method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Response;
+
 class HomeController extends Controller
 {
 
@@ -17,5 +21,17 @@ class HomeController extends Controller
         return view('home');
     }
 
+    public function duskStatus(Request $request){
+        $path = exec("pwd");
+        $artisan = file_exists($path . '/artisan') ? 'php artisan' : 'cd .. && php artisan';
+        exec($artisan . " dusk:chrome-driver --detect 2>&1", $out, $ret);
+
+        foreach ($out as $o){
+            if(!mb_strpos($o, 'ErrorException')){
+                return Response::json(['status' => true, 'out' => $out, 'ret' => $ret], 500); // Status code here
+            }
+        }
+        return Response::json(['status' => true, 'out' => $out, 'ret' => $ret], 200); // Status code here
+    }
 
 }
