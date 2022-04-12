381309104a739d05f1106c3997a47ade9e00fc4e
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-04-12 13:06:51 -0700

    updates probe check

--- probe-check.sh
+++ probe-check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $(php /var/www/html/artisan dusk:chrome-driver --detect | grep -v grep | grep Exception | wc -l) -lt 2 ]; then
+if [ $(php /var/www/html/artisan dusk:chrome-driver --detect | grep -v grep | grep -c Exception) -ge 1 ]; then
   exit 1
 else
   exit 0
