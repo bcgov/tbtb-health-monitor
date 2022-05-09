6a950188ce94e9b23ed4894be2b57708360138fa
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-05-09 13:54:41 -0700

    forces application to response 500 if vendor is not installed.

--- probe-check.sh
+++ probe-check.sh
@@ -3,5 +3,10 @@
 if [ $(php /var/www/html/artisan dusk:chrome-driver --detect | grep -v grep | grep -c Exception) -ge 1 ]; then
   exit 1
 else
+    ### Check if vendor directory does not exist ###
+    ### composer did not install ###
+    if [ ! -d "/var/www/html/vendor" ]; then
+        exit 1
+    fi
   exit 0
 fi
