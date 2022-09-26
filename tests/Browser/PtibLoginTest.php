1237b000f179b7c66f04120a8e08455b48799911
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-09-26 11:13:38 -0700

    fixes ptip admin login test

--- tests/Browser/PtibLoginTest.php
+++ tests/Browser/PtibLoginTest.php
@@ -20,7 +20,7 @@ class PtibLoginTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
 //            $browser->visit('https://logon.gov.bc.ca/clp-cgi/int/logon.cgi?TARGET=https://admin.privatetraininginstitutions.gov.bc.ca/&flags=1100:1,7&toggle=1');
-            $browser->visit('https://logon.gov.bc.ca/clp-cgi/int/logon.cgi?TARGET=https://admin.privatetraininginstitutions.gov.bc.ca/&flags=1100:1,7&toggle=1');
+            $browser->visit('https://logon7.gov.bc.ca/clp-cgi/int/logon.cgi?TARGET=https://admin.privatetraininginstitutions.gov.bc.ca/&flags=1100:1,7&toggle=1');
             $browser->screenshot(time() . "-testProdPtipAdminLoginPage-screenshot");
             $browser->type('input[id="user"]', env('PROD_PTIP_ADMIN_LOGIN_USERID'));
             $browser->type('input[id="password"]', env('PROD_PTIP_ADMIN_LOGIN_PASS'));
